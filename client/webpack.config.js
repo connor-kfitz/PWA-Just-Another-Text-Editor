4ba09120834a5fdecb9acee87a94cdfe979c2c24
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-15 11:08:30 -0400

    Added webpack plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,6 +18,27 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'TE',
+        description: 'Keep track of text',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        // icons: [
+        //   {
+        //     src: path.resolve('assets/images/logo.png'),
+        //     sizes: [96, 128, 192, 256, 384, 512],
+        //     destination: path.join('assets', 'icons'),
+        //   },
+        // ],
+      }),
       
     ],
 
