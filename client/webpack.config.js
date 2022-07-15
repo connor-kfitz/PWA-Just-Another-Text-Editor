ff6ec0d9f6b61851787ade68b7c4e919af0fddbb
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-07-15 11:15:38 -0400

    Added CSS Webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,6 +44,10 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
         
       ],
     },
